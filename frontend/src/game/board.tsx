2ee1656f86af95a14257f2c0e32a8c3614cf6bbc
JoschkaEB <s235220@dtu.dk>
2026-09-25 17:45:48 +0200

    Tile placement logic

--- frontend/src/game/board.tsx
+++ frontend/src/game/board.tsx
@@ -7,14 +7,28 @@ const board = (async () => {
 
 
     const background_img = await Assets.load('Assets/Sprites/slots_background.png');
-    const logo = await Assets.load('Assets/Sprites/pirots_logo_650.png');
-    const sprite = new Sprite(logo);
     const background_sprite = new Sprite(background_img);
-    sprite.anchor.set(0.5);
-    sprite.x = app.screen.width / 2;
-    sprite.y = app.screen.height / 2;
     app.stage.addChild(background_sprite);
-    app.stage.addChild(sprite);
+
+    const board_size = app.screen.width * 0.5;
+    const tile_size = board_size / 8;
+    const board_x_start = app.screen.width * 0.25;
+    const board_y_start = (app.screen.height / 2) - (board_size / 2);
+    const tile_img = await Assets.load('Assets/Sprites/slots_tile.png');
+    const tiles = [];
+
+    // tile placing logic
+    for (let y = 0; y < 6; y++) {
+        for (let x = 0; x < 6; x++) {
+            const tile = new Sprite(tile_img);
+            tile.width = tile_size;
+            tile.height = tile_size;
+            tile.position.x =  board_x_start + x * tile_size + tile_size;
+            tile.position.y =  board_y_start + y * tile_size + tile_size;
+            app.stage.addChild(tile);
+            tiles.push(tile);
+            }
+    }
 })();
 
 export default board;
